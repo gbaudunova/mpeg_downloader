dce4f9379d28aa7beecbe0b157f6b532fa52da3f
Gulbustan <gbaudunova@gmail.com>
2017-10-19 15:54:34 +0600

    Audiodownload

--- converter/tasks.py
+++ converter/tasks.py
@@ -1,10 +1,9 @@
 from __future__ import unicode_literals
-import youtube_dl
 from celery import shared_task
-from django.core.mail import send_mail
+# from django.core.mail import send_mail
 from ytconverter import settings
-
-
+import youtube_dl
+import ffmpy
 
 
 @shared_task
@@ -15,40 +14,54 @@ def download(url, email):
 
 
     ydl_opts = {
-        # 'format': 'bestaudio/best',
-        'outtmpl': settings.MEDIA_ROOT + '%(id)s',
+        'format': 'bestaudio/best',
+        'outtmpl': settings.MEDIA_ROOT + '/mp3/%(id)s.%(ext)s',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': '192',
         }],
+        'extractaudio' : True,
         'noplaylist': True,
         'progress_hooks': [my_hook],
     }
+
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         result = ydl.extract_info(url, download=True)
+        name = result.get('id', None)
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
 
+
+
+
+# def convert(name):
+#         ff = ffmpy.FFmpeg(
+#             inputs={settings.MEDIA_URL + 'mp3/%s' % name: None},
+#             outputs={settings.MEDIA_URL + 'mp3/': '-b:a 128k'}
+#         )
+#         ff.run()
 
     mp3 = '{domain}{path}{filename}{format}'.format(
         domain='http://127.0.0.1:8000',
         path=settings.MEDIA_URL,
-        filename='77',
-        format='mp4.mp3')
+        filename=name,
+        format='mp3')
 
-    send(mp3, email)
+    # send(mp3, email)
 
-
-
-def send(mp3, email):
-    print(mp3)
-    print(email)
-    send_mail('Mp3 downloading',
-              '%s' % (mp3),
-              'ivanovaanna038@gmail.com',
-              [email])
-              # fail_silently=False)
-
-    send()
+#
+#
+# def send(mp3, email):
+#     print(mp3)
+#     print(email)
+#     send_mail('Mp3 downloading',
+#               '%s' % (mp3),
+#               'ivanovaanna038@gmail.com',
+#               [email])
+#               # fail_silently=False)
+#
+#     send()
 
 
 
