6bc8352484b7ef246841f539464fe45e37d56606
Gulbustan <gbaudunova@gmail.com>
2017-11-06 11:23:51 +0600

    Make some changes in settings

--- ytconverter/settings.py
+++ ytconverter/settings.py
@@ -137,8 +137,8 @@ CELERYBEAT_SCHEDULER = "djcelery.schedulers.DatabaseScheduler"
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'annaroot16@gmail.com'
-EMAIL_HOST_PASSWORD = 'rootroot'
+EMAIL_HOST_USER = '#'
+EMAIL_HOST_PASSWORD = '#'
 EMAIL_USE_TLS = True
 
 
